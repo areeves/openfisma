7704ec1ec2c8ec9d29d4d5ae97910a0367182642
xhorse <unknown@reyosoft.com>
2008-05-27 13:37:15 +0000

    initial passwd and last login time

--- public/install/db/init_data.sql
+++ public/install/db/init_data.sql
@@ -260,7 +260,7 @@ INSERT INTO `PLUGINS` VALUES
 
 /* default users */
 INSERT INTO `USERS` VALUES 
-(17,'root','9d1fee901b933a42978f2eacbcddff65',NULL,NULL,NULL,'Application Administrator','Admin',NULL,'User','2006-11-28 08:49:16','2008-02-07 11:27:49',':9d1fee901b933a42978f2eacbcddff65:7b24afc8bc80e548d66c4e7ff72171c5','2008-02-07 11:35:07','0000-00-00 00:00:00',1,0,'555-555-5555','555-555-5555','admin@openfisma.org',10,'root_r');
+(17,'root','63a9f0ea7bb98050796b649e85481845',NULL,NULL,NULL,'Application Administrator','Admin',NULL,'User','2006-11-28 08:49:16','2008-02-07 11:27:49',':9d1fee901b933a42978f2eacbcddff65:7b24afc8bc80e548d66c4e7ff72171c5','0000-00-00 00:00:00','0000-00-00 00:00:00',1,0,'555-555-5555','555-555-5555','admin@openfisma.org',10,'root_r');
 
 
 INSERT INTO `configurations` VALUES 
