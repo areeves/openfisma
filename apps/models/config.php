04ccecdf14b1ef6338b5692a37765acd6b7aa1b1
Ryan Yang <ryan.yang@reyosoft.com>
2008-05-27 05:26:39 +0000

    config model (missing from the last version)

---
+++ apps/models/config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* OpenFISMA
+*
+* MIT LICENSE
+*
+* @version $Id$
+*/
+
+require_once MODELS . DS . 'Abstract.php';
+
+class Config extends Fisma_Model
+{
+    protected $_name = 'configurations';
+    protected $_primary = 'id';
+}
+
