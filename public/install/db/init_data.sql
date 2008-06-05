d5d7c2f317403c5438541a0f25c24e53921caa48
chriszero <unknown@reyosoft.com>
2008-06-05 03:48:48 +0000

    Adjust users table record and add init records to evaluation table

--- public/install/db/init_data.sql
+++ public/install/db/init_data.sql
@@ -1,6 +1,13 @@
-INSERT INTO `USERS` VALUES 
-(17,'root','63a9f0ea7bb98050796b649e85481845',NULL,NULL,NULL,'Application Administrator','Admin',NULL,'User','2006-11-28 08:49:16','2008-02-07 11:27:49',':9d1fee901b933a42978f2eacbcddff65:7b24afc8bc80e548d66c4e7ff72171c5','0000-00-00 00:00:00','0000-00-00 00:00:00',1,0,'555-555-5555','555-555-5555','admin@openfisma.org',10,'root_r');
-
+INSERT INTO `users` VALUES 
+(17,'root','202cb962ac59075b964b07152d234b70','Application Administrator','Admin',NULL,'User','0000-00-00 00:00:00','0000-00-00 00:00:00',':9d1fee901b933a42978f2eacbcddff65:7b24afc8bc80e548d66c4e7ff72171c5','0000-00-00 00:00:00','0000-00-00 00:00:00',1,0,'555-555-5555','555-555-5555','admin@openfisma.org','root_r');
 
 INSERT INTO `configurations` VALUES 
 (1,'max_absent_time','90','Absent days of an account causing being disabled');
+
+INSERT INTO `evaluations` (`id`, `name`, `precedence_id`, `function_id`, `group`) VALUES 
+(1, 'EV_SSO', 0, 0, 1),
+(2, 'EV_FSA', 1, 0, 1),
+(3, 'EV_IVV', 2, 0, 1),
+(4, 'EST', 0, 0, 2),
+(5, 'SSO', 4, 0, 2);
+
