cfaafb88fe30f48d251e9bfde5a8077fb4bc22f5
chriszero <unknown@reyosoft.com>
2008-06-12 02:55:08 +0000

    1.Add file MIGRATION_GUIDE for migration usage. 2.Remove a insert sql to avoid conflict when migration.php runs

--- public/install/db/init_data.sql
+++ public/install/db/init_data.sql
@@ -1,5 +1,3 @@
-INSERT INTO `users` VALUES 
-(17,'root','202cb962ac59075b964b07152d234b70','Application Administrator','Admin',NULL,'User','0000-00-00 00:00:00','0000-00-00 00:00:00',':9d1fee901b933a42978f2eacbcddff65:7b24afc8bc80e548d66c4e7ff72171c5','0000-00-00 00:00:00','0000-00-00 00:00:00',1,0,'555-555-5555','555-555-5555','admin@openfisma.org','root_r');
 
 INSERT INTO `configurations` VALUES 
 (1,'max_absent_time','90','Absent days of an account causing being disabled');
