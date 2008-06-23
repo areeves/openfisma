37fb2469de772202ec588df3873eec19646b1aaf
xhorse <unknown@reyosoft.com>
2008-06-23 08:37:23 +0000

    Fix: wrong data

--- public/install/db/init_data.sql
+++ public/install/db/init_data.sql
@@ -6,7 +6,7 @@ INSERT INTO `evaluations` (`id`, `name`, `precedence_id`, `function_id`, `group`
 (1, 'EV_SSO', 0, 0, 'EVIDENCE'),
 (2, 'EV_FSA', 1, 0, 'EVIDENCE'),
 (3, 'EV_IVV', 2, 0, 'EVIDENCE'),
-(4, 'EST', 0, 0, 'ACTION'),
+(4, 'EST', 3, 0, 'ACTION'),
 (5, 'SSO', 4, 0, 'ACTION');
 
 /* default user roles setup in openfisma */
