f1aaff48900aa45f6112f32d377af29526981a1c
xhorse <unknown@reyosoft.com>
2008-06-23 01:56:02 +0000

    fix:missing evidence model

---
+++ apps/models/evidence.php
@@ -0,0 +1,15 @@
+<?php
+/**
+* OpenFISMA
+*
+* MIT LICENSE
+*
+* @version $Id$
+*/
+require_once 'Abstract.php';
+class Evidence extends Fisma_Model
+{
+    protected $_name = 'evidences';
+    protected $_primary = 'id';
+}
+
