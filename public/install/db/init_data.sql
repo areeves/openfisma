52a14a3d93bd6482fb1f4f2ea9e4fe3a6bc6e3e0
Ryan Yang <ryan.yang@reyosoft.com>
2008-06-27 03:36:06 +0000

    correct configurations page view

--- public/install/db/init_data.sql
+++ public/install/db/init_data.sql
@@ -1,11 +1,11 @@
 
 INSERT INTO `configurations` VALUES 
-(1,'max_absent_time','90','Absent days of an account causing being disabled');
+(1,'max_absent_time','90','Maximum Days An Account Can Be Inactive');
 
 INSERT INTO `evaluations` (`id`, `name`, `precedence_id`, `function_id`, `group`) VALUES 
-(1, 'EV_SSO', 0, 0, 'EVIDENCE'),
-(2, 'EV_FSA', 1, 0, 'EVIDENCE'),
-(3, 'EV_IVV', 2, 0, 'EVIDENCE'),
+(1, 'EV_SSO', 0, 25, 'EVIDENCE'),
+(2, 'EV_FSA', 1, 26, 'EVIDENCE'),
+(3, 'EV_IVV', 2, 27, 'EVIDENCE'),
 (4, 'EST', 3, 0, 'ACTION'),
 (5, 'SSO', 4, 0, 'ACTION');
 
