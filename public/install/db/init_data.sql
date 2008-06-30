8f58602bc38d459937bd0eb9a8856fbfd4ad5b69
chriszero <unknown@reyosoft.com>
2008-06-30 02:35:34 +0000

    Fill out function_id (EST,SSO) in init_data.sql

--- public/install/db/init_data.sql
+++ public/install/db/init_data.sql
@@ -6,6 +6,5 @@ INSERT INTO `evaluations` (`id`, `name`, `precedence_id`, `function_id`, `group`
 (1, 'EV_SSO', 0, 25, 'EVIDENCE'),
 (2, 'EV_FSA', 1, 26, 'EVIDENCE'),
 (3, 'EV_IVV', 2, 27, 'EVIDENCE'),
-(4, 'EST', 3, 0, 'ACTION'),
-(5, 'SSO', 4, 0, 'ACTION');
-
+(4, 'EST', 3, 21, 'ACTION'),
+(5, 'SSO', 4, 24, 'ACTION');
