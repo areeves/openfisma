07bb37b9cfc079d17c34061aeb5a4958dafc22e2
Nathan R. Harris <nathan.harris@endeavorsystems.com>
2009-08-10 05:30:45 +0000

    first draft of crud functionality for Categories and Sub Categories in in place

--- application/controllers/MenuController.php
+++ application/controllers/MenuController.php
@@ -90,8 +90,8 @@ class MenuController extends SecurityController
             $incidents->add(new Fisma_Yui_MenuItem('Incident Dashboard', '/panel/incident/dashboard'));
             $incidents->add(new Fisma_Yui_MenuItem('Search', '/panel/incident/sub/search'));
             $incidents->add(new Fisma_Yui_MenuItem('Report Incident', '/panel/incident/sub/create'));
-            $incidents->add(new Fisma_Yui_MenuItem('Manage Categories', '/panel/category/sub/list'));
-            $incidents->add(new Fisma_Yui_MenuItem('Manage Workflows', '/panel/workflow/sub/list'));
+            $incidents->add(new Fisma_Yui_MenuItem('Manage Categories', '/panel/ircategory/sub/list'));
+            $incidents->add(new Fisma_Yui_MenuItem('Manage Workflows', '/panel/irworkflow/sub/list'));
             $incidents->add(new Fisma_Yui_MenuItem('Incident Reports', '/panel/incident/sub/reports'));
             
 
