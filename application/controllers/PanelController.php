d4c6ad1d835af506bdd38c9ad2aacc759ddc494f
Nathan R. Harris <nathan.harris@endeavorsystems.com>
2009-09-01 05:35:06 +0000

    Feature request on the IR branch: OFJ-3 Incident Reporting Module
    Updated to accomidate changes in incicdnet controller

--- application/controllers/PanelController.php
+++ application/controllers/PanelController.php
@@ -95,7 +95,7 @@ class PanelController extends SecurityController
     public function incidentAction()
     {
         $req = $this->getRequest();
-        $sub = $req->getParam('sub', 'searchbox');
+        $sub = $req->getParam('sub');
         $this->_helper->actionStack($sub, 'Incident');
         $this->_helper->actionStack('header');
     }
