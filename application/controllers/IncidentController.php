0a07ce8c3279082e93e5d1fd0a38c3b8a8541231
Nathan R. Harris <nathan.harris@endeavorsystems.com>
2009-09-01 05:34:21 +0000

    Feature request on the IR branch: OFJ-3 Incident Reporting Module
    Incident workflow functionality is basically in place now

--- application/controllers/IncidentController.php
+++ application/controllers/IncidentController.php
@@ -50,7 +50,477 @@ class IncidentController extends BaseController
     {
         parent::init();
     }
+   
+
+
+
+    public function dashboardAction() 
+    {
+        Fisma_Acl::requirePrivilege('incident', 'read'); 
+
+        $value = trim($this->_request->getParam('keywords'));
+        empty($value) ? $link = '' : $link = '/keywords/' . $value;
+        
+        $this->view->assign('pageInfo', $this->_paging);
+        $this->view->assign('link', $link);
+        
+        $this->render('dashboard');
+    }   
+
+    public function viewAction() 
+    { 
+        $incident_id = $this->_request->getParam('id');
+        $this->view->assign('id', $incident_id);
+        
+        $q  = Doctrine_Query::create()
+            ->select('i.*')
+            ->from('incident i')
+            ->where('i.id = ?', $incident_id);
+
+        $incident = $q->execute();
+
+        $this->view->assign('incident', $incident);
+
+        $incident = $incident->toArray();
+        $status = $incident[0]['status'];
     
+        if ($status == 'open') {
+            
+            $this->render('workflow');
+
+        } elseif ($status == 'new') {
+            $form = Fisma_Form_Manager::loadForm('incident_classify');
+
+            foreach($this->_getCategories() as $id => $cat) {
+                $form->getElement('classification')
+                     ->addMultiOptions(array($id => $cat));
+            }
+
+            $element = new Zend_Form_Element_Hidden('id');
+            $element->setValue($incident_id);
+
+            $form->addElement($element);
+
+            $this->view->assign('form', $form);
+
+            $this->render('classify');
+        
+        } elseif (($status == 'resolved') || ($status == 'rejected')) {
+            $form = Fisma_Form_Manager::loadForm('incident_close');
+
+            $element = new Zend_Form_Element_Hidden('id');
+            $element->setValue($incident_id);
+            
+            $form->addElement($element);
+            
+            $q  = Doctrine_Query::create()
+                ->select('s.id')
+                ->from('irIncidentWorkflow s')
+                ->where('s.incidentId = ?', $incident_id)
+                ->andWhere('s.status = ?', 'current');
+ 
+            $step = $q->execute();
+            $step = $step->toArray();        
+
+            $element2 = new Zend_Form_Element_Hidden('step_id');
+            $element2->setValue($step[0]['id']);
+     
+            $form->addElement($element2);
+
+            $this->view->assign('form', $form);
+
+            $this->render('close');
+
+        } elseif($status == 'closed') {
+            $this->render('history');
+        }
+    }
+
+    public function workflowAction() {
+        $incident_id = $this->_request->getParam('id');
+        
+        $q  = Doctrine_Query::create()
+            ->select('iw.*')
+            ->from('irIncidentWorkflow iw')
+            ->where('iw.incidentId = ?', $incident_id);
+
+        $incident = $q->execute();
+
+        $this->view->assign('id', $incident_id);
+        $this->view->assign('incident', $incident->toArray());
+
+        $this->render('workflow-interface');
+    }
+
+    public function completestepAction() {
+        $incident_id = $this->_request->getParam('id');
+        $step_id     = $this->_request->getParam('step_id');
+        $comments    = $this->_request->getParam('comments');
+        
+        $step = new IrIncidentWorkflow();
+
+        /* update step just completed */
+        $step = $step->getTable()->find($step_id);
+        $step->status     = 'completed';
+        $step->comments   = $comments;
+        $step->userId     = Zend_Auth::getInstance()->getIdentity()->id;
+        $step->completeTs = date('Y-m-d H:i:s');
+        $step->save();
+        
+        /* update next step to make it current */
+        $step = $step->getTable()->find($step_id + 1);
+        $step->status     = 'current';
+        $step->save();
+
+        /* check for last step and set incident status to resolved */
+        $q  = Doctrine_Query::create()
+            ->select('count(*) as count')
+            ->from('irIncidentWorkflow iw')
+            ->where('iw.incidentId = ?', $incident_id)
+            ->andWhere('iw.status = ?', 'queued');
+
+        $step_count = $q->execute();
+        $step_count = $step_count->toArray();    
+    
+        if ($step_count['0']['count'] == 0) {
+            $incident = new Incident();
+            $incident = $incident->getTable()->find($incident_id);
+
+            $incident->status = 'resolved';
+
+            $incident->save();
+            
+            print 'redirect'; 
+            exit;
+        }
+
+        $this->_forward('workflow');
+    }
+
+    public function closeAction() {
+        $incident_id = $this->_request->getParam('id');
+        $step_id     = $this->_request->getParam('step_id');
+
+        $incident = new Incident();
+        $incident = $incident->getTable()->find($incident_id);
+
+        $incident->status = 'closed';
+        $incident->save();
+
+        $step = new IrIncidentWorkflow();
+        $step = $step->getTable()->find($step_id);
+
+        $step->status     = 'completed';
+        $step->comments   = $comments;
+        $step->userId     = Zend_Auth::getInstance()->getIdentity()->id;
+        $step->completeTs = date('Y-m-d H:i:s');
+
+        $step->save();
+            
+        $this->message('Incident Closed', self::M_NOTICE);
+        $this->_forward('dashboard');
+    }
+
+    public function classifyAction() {
+        $id            = $this->_request->getParam('id');
+        $subCategoryId = $this->_request->getParam('classification');
+        $comment       =  $this->_request->getParam('comment');
+
+        
+        if ($this->_request->getParam('reject') == 'reject') {
+            $this->message('Incident Rejected', self::M_NOTICE);
+            
+            $incident = new Incident();
+            $incident = $incident->getTable()->find($id);
+            $incident->status = 'rejected';
+            $incident->save();
+        
+            
+            /* Add rejected step to workflow table*/
+            $iw = new IrIncidentWorkflow();    
+           
+            $iw->incidentId  = $id; 
+            $iw->name        = 'Incident Rejected';
+            $iw->comments    = $comment;
+            $iw->sortorder   = 0;
+            $iw->userId      = Zend_Auth::getInstance()->getIdentity()->id;
+            $iw->completeTs = date('Y-m-d H:i:s');
+
+            $iw->status      = 'completed';
+
+            $iw->save();
+
+            /* Add final close step to incident workflow table*/
+            $iw = new IrIncidentWorkflow();    
+           
+            $iw->incidentId  = $id; 
+            $iw->name        = 'Close Incident';
+            $iw->sortorder   = 1;
+
+            $iw->status      = 'queued';
+
+            $iw->save();
+
+        } elseif ($this->_request->getParam('open') == 'open')  {
+            $this->message('Incident Opened', self::M_NOTICE);
+
+            /* update incident status and category */
+            $incident                 = new Incident();
+            $incident                 = $incident->getTable()->find($id);
+            $incident->status         = 'open';
+            $incident->classification = $subCategoryId;
+            
+            $incident->save();
+
+
+            /* Add opened step to workflow table*/
+            $iw = new IrIncidentWorkflow();    
+           
+            $iw->incidentId  = $id; 
+            $iw->name        = 'Incident Opened';
+            $iw->comments    = $comment;
+            $iw->sortorder   = 0;
+            $iw->userId      = Zend_Auth::getInstance()->getIdentity()->id;
+            $iw->completeTs = date('Y-m-d H:i:s');
+
+            $iw->status      = 'completed';
+
+            $iw->save();
+
+            /* create snapshot of workflow and add it to the ir_incident_workflow table */
+            $subcat = Doctrine::getTable('irSubCategory')->find($subCategoryId);
+           
+            $q = Doctrine_Query::create()
+                 ->select('s.id, s.roleid, s.sortorder, s.name, s.description')
+                 ->from('irSteps s')
+                 ->where('s.workflowid = ?', $subcat->workflowId)
+                 ->orderby('s.sortorder');
+                
+            $steps = $q->execute();
+ 
+            $steps = $steps->toArray();
+
+            foreach($steps as $step) {
+                $iw = new IrIncidentWorkflow();    
+               
+                $iw->incidentId  = $id; 
+                $iw->name        = $step['name'];
+                $iw->description = $step['description'];
+                $iw->sortorder   = $step['sortorder'];
+
+                $iw->status      = ($step['sortorder'] == 1) ? 'current' : 'queued';
+                                
+                $iw->save();
+            }
+
+            /* Add final close step to incident workflow table*/
+            $iw = new IrIncidentWorkflow();    
+           
+            $iw->incidentId  = $id; 
+            $iw->name        = 'Close Incident';
+            $iw->sortorder   = $step['sortorder'] + 1;
+
+            $iw->status      = 'queued';
+
+            $iw->save();
+        }
+
+        $this->_forward('view');
+    }
+
+    function commentsAction() {
+        $incident_id = $this->_request->getParam('id');
+        $this->view->assign('id', $incident_id);
+
+        $q  = Doctrine_Query::create()
+            ->select('c.*')
+            ->from('irComment c')
+            ->where('c.incidentId = ?', $incident_id)
+            ->orderBy('createdTs DESC');
+
+        $comments = $q->execute();
+
+        $this->view->assign('comments', $comments->toArray());
+
+        $this->render('comments');   
+    }
+
+    function addcommentAction() {
+        $incident_id = $this->_request->getParam('id');
+        $comments    = $this->_request->getParam('comments');
+        
+        $comment = new IrComment();
+
+        $comment->incidentId = $incident_id;
+        $comment->userId     = Zend_Auth::getInstance()->getIdentity()->id;
+        $comment->createdTs  = date('Y-m-d H:i:s');
+        $comment->comment    = $comments;
+
+        $comment->save();
+
+        $this->_forward('comments');
+    }
+
+
+    /**
+     * Returns the forms and lists for managing actors and viewers
+     *
+     * @return Zend_Form
+     */
+    public function actorAction() 
+    {
+        $id = $this->_request->getParam('id');
+
+        $q = Doctrine_Query::create()
+             ->select('u.id, u.nameFirst, u.nameLast')
+             ->from('user u')
+             ->where('u.id NOT IN (SELECT ia.userId FROM irIncidentActor ia WHERE ia.incidentid = ?)', $id)
+             ->andWhere('u.id NOT IN (SELECT io.userId FROM irIncidentObserver io WHERE io.incidentid = ?)', $id);
+
+        $users = $q->execute();
+
+        $this->view->assign('id',$id);
+        $this->view->assign('users',$users->toArray());
+
+        $q = Doctrine_Query::create()
+             ->select('u.id, u.nameFirst, u.nameLast')
+             ->from('user u')
+             ->where('u.id IN (SELECT ia.userId FROM irIncidentActor ia WHERE ia.incidentid = ?)', $id);
+
+        $users = $q->execute();
+
+        $this->view->assign('actors',$users->toArray());
+        
+        $q = Doctrine_Query::create()
+             ->select('u.id, u.nameFirst, u.nameLast')
+             ->from('user u')
+             ->where('u.id IN (SELECT io.userId FROM irIncidentObserver io WHERE io.incidentid = ?)', $id);
+
+        $users = $q->execute();
+
+        $this->view->assign('observers',$users->toArray());
+
+        $this->render('actors');
+    }
+
+    public function actoraddAction() {
+        $id     = $this->_request->getParam('id');
+        $userid = $this->_request->getParam('userid');
+        
+        $actor = new IrIncidentActor();
+
+        $actor->incidentId = $id;
+        $actor->userId = $userid;
+        $actor->save();
+
+
+        $this->_forward('actor'); 
+    }
+    
+    public function actorremoveAction() {
+        $id     = $this->_request->getParam('id');
+        $userid = $this->_request->getParam('userid');
+        
+        $q =    Doctrine_Query::create()
+                ->delete('irIncidentActor ia')
+                ->where('ia.userid = ?', $userid)
+                ->andWhere('ia.incidentid = ?', $id);
+        
+        $q->execute();
+
+        $this->_forward('actor'); 
+    }
+
+    public function observeraddAction() {
+        $id     = $this->_request->getParam('id');
+        $userid = $this->_request->getParam('userid');
+        
+        $actor = new IrIncidentObserver();
+
+        $actor->incidentId = $id;
+        $actor->userId = $userid;
+        $actor->save();
+
+
+        $this->_forward('actor'); 
+    }
+
+    public function observerremoveAction() {
+        $id     = $this->_request->getParam('id');
+        $userid = $this->_request->getParam('userid');
+        
+        $q =    Doctrine_Query::create()
+                ->delete('irIncidentObserver io')
+                ->where('io.userid = ?', $userid)
+                ->andWhere('io.incidentid = ?', $id);
+        
+        $q->execute();
+
+        $this->_forward('actor'); 
+    }
+ 
+    /**
+     * list the incidents from the search, 
+     * if search none, list all incidents
+     * 
+     */
+    public function searchAction()
+    {
+        Fisma_Acl::requirePrivilege('incident', 'read');
+        $value = trim($this->_request->getParam('keywords'));
+
+        $this->_helper->layout->setLayout('ajax');
+        $this->_helper->viewRenderer->setNoRender();
+        $sortBy = $this->_request->getParam('sortby', 'reporttsname');
+        $order = $this->_request->getParam('order');
+        $status = array($this->_request->getParam('status'));
+
+        if($status[0] == 'resolved') {
+            $status[] = 'rejected';
+        }  
+       
+        $organization = Doctrine::getTable('Incident');
+        if (!in_array(strtolower($sortBy), $organization->getColumnNames())) {
+            throw new Fisma_Exception('Invalid "sortBy" parameter');
+        }
+        
+        
+        $order = strtoupper($order);
+        if ($order != 'DESC') {
+            $order = 'ASC'; //ignore other values
+        }
+        
+        $q = Doctrine_Query::create()
+             ->select('*')
+             ->from('incident i')
+             ->whereIn('i.status', $status)
+             ->orderBy("i.$sortBy $order")
+             ->limit($this->_paging['count'])
+             ->offset($this->_paging['startIndex']);
+
+        if (!empty($value)) {
+            $incidentIds = Fisma_Lucene::search($value, 'incident');
+            if (empty($incidentIds)) {
+                $incidentIds = array(-1);
+            }
+            $q->whereIn('i.id', $incidentIds);
+        }
+        $totalRecords = $q->count();
+        $incidents = $q->execute();
+        
+        $tableData = array('table' => array(
+            'recordsReturned' => count($incidents->toArray()),
+            'totalRecords' => $totalRecords,
+            'startIndex' => $this->_paging['startIndex'],
+            'sort' => $sortBy,
+            'dir' => $order,
+            'pageSize' => $this->_paging['count'],
+            'records' => $incidents->toArray()
+        ));
+        
+        echo json_encode($tableData);
+    }
+
     
     /**
      * Returns the standard form for creating an incident
@@ -169,6 +639,8 @@ class IncidentController extends BaseController
         $subject->merge($values);
         $subject->save();
     }
+
+
     private function _getStates() {
         $states = array (
               'AL' => 'Alabama',
@@ -286,5 +758,20 @@ class IncidentController extends BaseController
         }
 
         return 1;
+    }
+
+    private function _getCategories() {
+        $q = Doctrine_Query::create()
+             ->select('s.id, s.name')
+             ->from('irSubCategory s')
+             ->orderBy("s.name");
+
+        $categories = $q->execute();
+        
+        foreach($categories->toArray() as $cat) {
+            $ret_val[$cat['id']] = $cat['name'];
+        }
+
+        return $ret_val;
     }
 }
