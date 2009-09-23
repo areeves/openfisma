93cfd6eded9d93366cbd6438d218f2b0df6db020
Joshua D. Boyd <joshua.boyd@endeavorsystems.com>
2009-09-23 19:35:37 +0000

    Improvement on the performance branch: OFJ-110 Minify/Combine JS/CSS
    Add phploader library for combo loading of YUI and OpenFISMA JS and CSS
    
    phploader can be updated as needed utilizing git

---
+++ library/phploader/phploader/test/testall.php
@@ -0,0 +1,18 @@
+<?PHP
+/**
+ *  Copyright (c) 2009, Yahoo! Inc. All rights reserved.
+ *  Code licensed under the BSD License:
+ *  http://developer.yahoo.net/yui/license.html
+ *  version: 1.0.0b1
+ */
+?>
+<html>
+<head>
+<?PHP include("loader-inc.php"); ?>
+</head>
+
+<body>
+</body>
+</html>
+
+<?PHP //header('Content-type: text/html'); ?>
