5b19eba8c6aa30e9ebb0fe1387e045a828033eeb
Joshua D. Boyd <joshua.boyd@endeavorsystems.com>
2009-10-28 03:47:23 +0000

    Admin Merge from trunk into performance branch, 2460:2481

--- library/Fisma.php
+++ library/Fisma.php
@@ -230,7 +230,20 @@ class Fisma
                     ini_set("xdebug.$param", $value);
                 }
             }
-            
+
+            // Timezone configuration
+            if (isset(self::$_appConf->timezone)) {
+                ini_set("date.timzeone", self::$_appConf->timezone);
+            }
+            else {
+                ini_set("date.timezone", "America/New_York");
+            }
+
+            // Log all PHP errors
+            ini_set('error_reporting', E_ALL | E_STRICT);
+            ini_set('log_errors', TRUE);
+            ini_set('error_log', self::$_rootPath . '/data/logs/php.log');
+
             // Session configuration
             $sessionOptions = self::$_appConf->session->toArray();
             $sessionOptions['save_path'] = self::$_rootPath . '/' . $sessionOptions['save_path'];
@@ -278,6 +291,7 @@ class Fisma
         $manager = Doctrine_Manager::getInstance();
         $manager->setAttribute(Doctrine::ATTR_USE_DQL_CALLBACKS, true);
         $manager->setAttribute(Doctrine::ATTR_USE_NATIVE_ENUM, true);
+	$manager->setAttribute(Doctrine::ATTR_AUTOLOAD_TABLE_CLASSES, true);
 
         // Set up the cache driver and connect to the manager.
         if(function_exists('apc_fetch')) {
@@ -293,7 +307,10 @@ class Fisma
                'data_fixtures_path'  =>  self::getPath('fixture'),
                'models_path'         =>  self::getPath('model'),
                'migrations_path'     =>  self::getPath('migration'),
-               'yaml_schema_path'    =>  self::getPath('schema')
+               'yaml_schema_path'    =>  self::getPath('schema'),
+               'generate_models_options' => array(
+                    'generateTableClasses' => true
+               )
         ));
     }
     
@@ -395,8 +412,11 @@ class Fisma
         while ($file = readdir($modelDir)) {
             if ($match = strpos($file, '.php')) {
                 $modelName = substr($file, 0, $match);
-                require_once(Fisma::getPath('model') . '/' . $file);
-                Doctrine::getTable($modelName)->getRecordListener()->setOption('disabled', !self::$_listenerEnabled);
+
+                if (!strstr($modelName, 'Table')) {
+                    require_once(Fisma::getPath('model') . '/' . $file);
+                    Doctrine::getTable($modelName)->getRecordListener()->setOption('disabled', !self::$_listenerEnabled);
+                }
             }
         }
     }
