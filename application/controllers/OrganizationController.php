6d659811fec1689e9e516dbd03be910f979f5cc0
Andrew Reeves <andrew.reeves@endeavorsystems.com>
2010-04-22 17:18:50 +0000

    Task on branches OFJ-605 Output JSON from view script
    Fixed JSON views in Organization controller: tree-data, search

--- application/controllers/OrganizationController.php
+++ application/controllers/OrganizationController.php
@@ -77,6 +77,8 @@ class OrganizationController extends SecurityController
         parent::init();
         $this->_helper->contextSwitch()
                       ->addActionContext('tree-data', 'json')
+                      ->addActionContext('search', 'json')
+                      ->setAutoJsonSerialization(false)
                       ->initContext();
     }
     
@@ -158,7 +160,7 @@ class OrganizationController extends SecurityController
     {
         Fisma_Acl::requirePrivilegeForClass('read', 'Organization');
         $value = htmlentities(trim($this->_request->getParam('keywords')));
-        empty($value) ? $link = '' : $link = '/keywords/' . $value;
+        $link = '/format/json' . (empty($value) ? '' : '/keywords/' . $value);
         $this->searchbox();
         $this->view->assign('pageInfo', $this->_paging);
         $this->view->assign('link', $link);
@@ -176,8 +178,6 @@ class OrganizationController extends SecurityController
         Fisma_Acl::requirePrivilegeForClass('read', 'Organization');
         $keywords = html_entity_decode(trim($this->_request->getParam('keywords')));
         
-        $this->_helper->layout->setLayout('ajax');
-        $this->_helper->viewRenderer->setNoRender();
         $sortBy = $this->_request->getParam('sortby', 'name');
         $order = $this->_request->getParam('order');
         
@@ -209,7 +209,7 @@ class OrganizationController extends SecurityController
         $totalRecords = $userOrgQuery->count();
         $organizations = $userOrgQuery->execute();
         
-        $tableData = array('table' => array(
+        $this->view->tableData = array('table' => array(
             'recordsReturned' => count($organizations->toArray()),
             'totalRecords' => $totalRecords,
             'startIndex' => $this->_paging['startIndex'],
@@ -218,8 +218,6 @@ class OrganizationController extends SecurityController
             'pageSize' => $this->_paging['count'],
             'records' => $organizations->toArray()
         ));
-        
-        echo Zend_Json::encode($tableData);
     }
     
     /**
