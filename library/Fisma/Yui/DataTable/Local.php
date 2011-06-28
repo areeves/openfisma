57e65df0e07f850b45a178602098aa416929d9c4
Dale Frey <dale.frey@endeavorsystems.com>
2011-06-28 15:01:27 -0400

    OFJ-956 Improve role-privilege assignment interface
    Fixes for code-review -
    - http://crucible.openfisma.org/cru/OFC-359#c2569
    - http://crucible.openfisma.org/cru/OFC-359#c2570
    - http://crucible.openfisma.org/cru/OFC-359#c2565
    - http://crucible.openfisma.org/cru/OFC-359#c2566
    - http://crucible.openfisma.org/cru/OFC-359#c2568
    - http://crucible.openfisma.org/cru/OFC-359#c2567
    - http://crucible.openfisma.org/cru/OFC-359#c2559
    - http://crucible.openfisma.org/cru/OFC-359#c2557
    - http://crucible.openfisma.org/cru/OFC-359#c2558
    - http://crucible.openfisma.org/cru/OFC-359#c2554
    - http://crucible.openfisma.org/cru/OFC-359#c2553
    - http://crucible.openfisma.org/cru/OFC-359#c2550
    - http://crucible.openfisma.org/cru/OFC-359#c2548
    - http://crucible.openfisma.org/cru/OFC-359#c2547
    - http://crucible.openfisma.org/cru/OFC-359#c2546
    - http://crucible.openfisma.org/cru/OFC-359#c2548
    - http://crucible.openfisma.org/cru/OFC-359#c2547
    - http://crucible.openfisma.org/cru/OFC-359#c2546
    - http://crucible.openfisma.org/cru/OFC-359#c2545
    - http://crucible.openfisma.org/cru/OFC-359#c2549
    - http://crucible.openfisma.org/cru/OFC-359#c2544
    - http://crucible.openfisma.org/cru/OFC-359#c2543
    - http://crucible.openfisma.org/cru/OFC-359#c2542
    - http://crucible.openfisma.org/cru/OFC-359#c2539
    - http://crucible.openfisma.org/cru/OFC-359#c2538
    - http://crucible.openfisma.org/cru/OFC-359#c2536
    - http://crucible.openfisma.org/cru/OFC-359#c2537
    - http://crucible.openfisma.org/cru/OFC-359#c2535
    - http://crucible.openfisma.org/cru/OFC-359#c2534

--- library/Fisma/Yui/DataTable/Local.php
+++ library/Fisma/Yui/DataTable/Local.php
@@ -39,9 +39,9 @@ class Fisma_Yui_DataTable_Local extends Fisma_Yui_DataTable_Abstract
      * An array of events for the data-table to listen for, and the javascript function name to tigger for each
      * i.e. eventListeners['checkboxClickEvent'] => 'myJavaScriptFunctionName'
      * 
-     * @var 
+     * @var array 
      */
-    public $eventListeners = array();
+    private $_eventListeners = array();
     
     /**
      * Set the table's data
@@ -75,7 +75,7 @@ class Fisma_Yui_DataTable_Local extends Fisma_Yui_DataTable_Abstract
             'data' => $this->_data,
             'columnDefinitions' => $this->_getYuiColumnDefinitions(),
             'responseSchema' => $this->_getYuiResponseSchema(),
-            'eventListeners' => $this->eventListeners
+            'eventListeners' => $this->_eventListeners
         );
         
         return $view->partial('yui/data-table-local.phtml', 'default', $data);
@@ -98,4 +98,14 @@ class Fisma_Yui_DataTable_Local extends Fisma_Yui_DataTable_Abstract
         
         return $responseSchema;
     }        
+    
+    /**
+     * Adds an event listener to the YUI data table, upon event, will call the target JavaScript function.
+     * 
+     * @return void
+     */
+    public function addEventListener($yuiEventName, $javaScriptFunctionName)
+    {
+        $this->_eventListeners[$yuiEventName] = $javaScriptFunctionName;
+    }
 }
