8cddd508eb3d6a354b5caeceedf645418d05e8c7
Ben Zheng <ben.zheng@reyosoft.com>
2011-09-08 09:33:28 +0800

    OFJ-1495 The undefined words show at user edit page when check the account who is not LDAP user under LDAP
    Change the code to ensure data.accountInfo array is not empty and the value of columnValue is not undefine.

--- public/javascripts/combined.js
+++ public/javascripts/combined.js
@@ -12456,7 +12456,7 @@ Fisma.User = {
                                                 'title');
 
                     // Make sure each column value is not null in LDAP account, then populate to related elements.
-                    if (data.accountInfo !== null) {
+                    if (data.accountInfo.length > 0) {
                         for (var i in ldapColumns) {
                             if (!ldapColumns.hasOwnProperty(i)) {
                                 continue;
@@ -12464,7 +12464,7 @@ Fisma.User = {
 
                             var columnValue = data.accountInfo[ldapColumns[i]];
 
-                            if (columnValue !== null) {
+                            if (!YAHOO.lang.isUndefined(columnValue)) {
                                 document.getElementById(openfismaColumns[i]).value = columnValue;
                             } else {
                                 document.getElementById(openfismaColumns[i]).value = '';
